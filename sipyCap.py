f8fa8d00e2302968575022abc99eb750da445d2b
John Brown <root@localhost.localdomain>
2017-07-12 17:18:10 +0100

    returns list of dictionaries containing SDP port and IP, src ip, dst ip

--- sipyCap.py
+++ sipyCap.py
@@ -2,15 +2,15 @@
 
 import pyshark
 
-input_callid = '191f6237850fd0f51352749e35e496cd4750ee@212.165.21.6'
-cap = pyshark.FileCapture('example1.pcap', display_filter='sip')
+input_callid = '191f6237850fd0f51352749e35e496cd4750ee'
+cap = pyshark.FileCapture('example1.pcap')
 sdp = {}
 cseq = []
 
 def find_call(callid):
     for pkt in cap:
         try:
-            if pkt.sip.call_id == callid:
+            if callid in pkt.sip.call_id:
                 if pkt.sip.method == 'OPTION':
                     pass
                 else:
@@ -34,36 +34,57 @@ def find_cseq(callid):
         except AttributeError as e:
             pass
 
-def find_pkt_media(leg,cseq):
 
+
+# extract_media will return a list containing all the sets of media information
+# m = extract_media(callid)
+# print(m)
+# [ {ip: 172.168.0.1, port: 7941},
+#   {ip: 172.168.0.2, port: 8461}]
+
+
+def extract_media(callid):
+    media = []
     for pkt in cap:
         try:
-            if (pkt.sip.cseq_seq = cseq and pkt.sip.sdp_media_port):
-                port = pkt.sip.sdp_media_port
-                ip = pkt.sip.sdp_media_ip
-                
-
-            pkt.sip.sdp_media_port
-#            a['port'] = pkt.sip.sdp_media_port
-#            a['ip'] = pkt.sip.sdp_connection_info_address
-            srce = pkt.ip.src
-            dest = pkt.ip.dst
-            cseq = pkt.sip.cseq
-            print(dest + ' -> ' + srce + ':' + cseq)
-#            print('IP: ' + a['ip'])
-#            print('Port: ' + a['port'])
+            if callid in pkt.sip.call_id:
+                media.append({
+                    "ip": pkt.sip.sdp_connection_info_address,
+                    "port": pkt.sip.sdp_media_port,
+                    "src_ip": pkt.ip.src,
+                    "dst_ip": pkt.ip.dst,
+                    }
+                    )
         except AttributeError as e:
             pass
 
+    return(media)
+
+
+def groups_channels_by_legs(ungrouped_media):
+    for x in len(ungrouped_media):
+        print(x)
+
+
+    
+
 if find_call(input_callid):
     print('call found')
-    find_cseq(input_callid)
-    find_leg_media(input_callid)
-    for x in cseq:
-        print(x)
 else:
     print('call not found')
+    exit
 
-#for pkt in cap:
-#    print(pkt.sip.cseq)
+call_media = extract_media(input_callid)
+for media in call_media:
+    print(media)
 
+
+
+
+
+"""
+    find_cseq(input_callid)
+    find_pkt_media(input_callid)
+    for x in cseq:
+        print(x)
+        """
